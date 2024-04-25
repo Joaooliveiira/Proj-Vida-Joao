dfd5f345b754dd9b439c286d79a6b0c7b22091af
Joaooliveiira <108005769+Joaooliveiira@users.noreply.github.com>
2024-04-25 17:30:35 -0300

    25/04/24

--- main.js
+++ main.js
@@ -7,31 +7,41 @@ for (let i = 0; i < botoes.length; i++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo")
         }
+
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
 
-const contadores = document.querySelectorALL (".contador");
-const tempoObjetivo1 = new Date("2024-07-08T00:00:00");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-31-08T00:00:00");
+const tempoObjetivo2 = new Date("2024-31-08T00:00:00");
+const tempoObjetivo3 = new Date("2024-31-08T00:00:00");
+const tempoObjetivo4 = new Date("2024-31-08T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual; 
-    let segundos = Math . floor(tempoFinal/1000);
-    let minutos = math.floor(segundos/60);
+    let segundos = Math . floor(tempoFinal/1000);  658957412
+    let minutos = Math.floor(segundos/60);
     let horas = Math.floop(minutos/60);
     let dias = Math.floop(horas/24);
 
 
-    segundos%= 60;
+    segundos %= 60;
     minutos%= 60;
     horas%=24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos "
-
-
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 
 }
+
+function atualizaCronometro(){
+    atualizarCronometro();
+    setInterval(atualizarCronometro, 1000);
+}
+
+comecaCronometro();
